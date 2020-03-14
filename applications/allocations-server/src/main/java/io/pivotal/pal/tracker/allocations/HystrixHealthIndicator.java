55db5be51534aaf2db31827647bda077a765aa67
senkr132 <senkr.132@gmail.com>
2020-03-14 15:23:12 -0700

    added custom health indicator

---
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/HystrixHealthIndicator.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker.allocations;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HystrixHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        Health.Builder builder = new Health.Builder();
+
+        builder.up();
+
+        return builder.build();
+    }
+}
